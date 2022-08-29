d3dae6a726bd7ca5b50c3862d2c2a7c9556378dc
prynce0319 <princebensmith15@gmail.com>
2022-08-29 16:59:34 +0000

    1st update

--- config/app.php
+++ config/app.php
@@ -182,7 +182,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Gloudemans\Shoppingcart\ShoppingcartServiceProvider::class,
-        Unicodeveloper\Paystack\PaystackServiceProvider::class,
+        
         
 
         /*
