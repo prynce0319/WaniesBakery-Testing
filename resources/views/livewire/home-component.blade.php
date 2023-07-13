bdcb9f51c002574e92c38085e4771146f73e006e
prince <princebensmith15@gmail.com>
2023-07-13 15:20:55 +0000

    home product image fix

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -87,7 +87,7 @@
                                     <div class="product product-style-2 equal-elem ">
                                         <div class="product-thumnail">
                                             <a href="{{ route('product.details',['slug'=>$lproduct->slug]) }}" title="{{ $lproduct->name }}">
-                                                <figure><img src="{{ asset('assets/images/products')}}/{{ $lproduct->image }}" width="800" height="800" alt="{{ $lproduct->name }}"></figure>
+                                                <figure><img src="{{ asset('/products')}}/{{ $lproduct->image }}" width="800" height="800" alt="{{ $lproduct->name }}"></figure>
                                             </a>
                                         </div>
                                         <div class="product-info">
